22d0723c193a825152fd01157662c756e8d89b07
Mathew B <bogaert@email.arizona.edu>
2020-12-11 16:23:32 -0700

    notes api has notes displaying

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const { db } = require('./Develop/db/db.json');
+const db = require('./Develop/db/db.json');
+console.log("db: ", db);
 
 // GET /notes should return the notes.html file
 
