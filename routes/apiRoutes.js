30ac449f779dd0608e3cb21b4c0ea36592e0fe91
Mathew B <bogaert@email.arizona.edu>
2020-12-13 12:51:21 -0700

    working notes page, thanks Wesley in office hours

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,25 @@
 
 const router = require('express').Router();
-const db = require('../../db/db.json');
+const db = require('../db/db.json');
 const fs = require('fs');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON
 router.get('/notes', (req, res) => {
+    console.log(db);
     res.json(db);
 });
 
-// POST /api/notes should receive a new note ans save to req.body
+// POST /api/notes should receive a new note and save to req.body
 // then add it to the db.json file, then return the new note to the client
 router.post('/notes', (req, res) => {
-    // creates an id based on the length of the array for each new note - will need to change later
+    // creates an id based on the length of the array for each new note
+    // need to change to uuid
     req.body.id = db.length.toString();
     const note = req.body;
     db.push(note);
 
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(db));
-    console.log(db)
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    console.log("Added to notes: ", db);
     res.json(db);
 });
 
