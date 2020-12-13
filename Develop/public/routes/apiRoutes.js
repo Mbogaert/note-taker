00612abd76c1294b7606c273a9767b12f9d292b7
Mathew B <bogaert@email.arizona.edu>
2020-12-13 11:21:19 -0700

    cleaned up apiRoutes

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON
 router.get('/notes', (req, res) => {
-    console.log(db);
     res.json(db);
 });
 
@@ -14,16 +13,12 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     // creates an id based on the length of the array for each new note - will need to change later
     req.body.id = db.length.toString();
-    // add notes to json file
     const note = req.body;
+    db.push(note);
 
-    // read the notes...
-    let notes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "UTF-8"));
-    notes.push(note);
-
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
-    console.log(notes)
-    res.json(notes);
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(db));
+    console.log(db)
+    res.json(db);
 });
 
 module.exports = router;
